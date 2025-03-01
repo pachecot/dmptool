63880ea155ea961953cadb2ff7127436e25212cf
Tom Pacheco <tomp@netp.org>
2025-03-01 09:21:42 -0500

    feat(ref): add CSV and XLSX export functionality for references

--- cmds/ref/ref.go
+++ cmds/ref/ref.go
@@ -1,15 +1,24 @@
 package ref
 
 import (
+	"encoding/csv"
 	"fmt"
 	"io"
+	"log"
 	"os"
+	"path"
 	"slices"
 	"strings"
 	"unicode"
 
 	"github.com/tpacheco/dmptool/dmp"
+	"github.com/xuri/excelize/v2"
 	"golang.org/x/exp/maps"
+)
+
+const (
+	csvExt  = ".csv"
+	xlsxExt = ".xlsx"
 )
 
 type refHandler struct {
@@ -121,130 +130,211 @@ func (cmd *Command) Execute() {
 		})
 	}
 
-	w := os.Stdout
-
-	if cmd.OutFile != "" {
-		var err error
-		w, err = os.Create(cmd.OutFile)
-		if err != nil {
-			fmt.Println("could not create file")
-			return
-		}
-		defer func() {
-			w.Sync()
-			w.Close()
-		}()
+	if len(refs) == 0 {
+		fmt.Println("No references found.")
+		return
 	}
 
 	if cmd.Bare {
+		w := os.Stdout
+		if cmd.OutFile != "" {
+			var err error
+			w, err = os.Create(cmd.OutFile)
+			if err != nil {
+				fmt.Println("could not create file")
+				return
+			}
+			defer func() {
+				w.Sync()
+				w.Close()
+			}()
+		}
+
 		for _, v := range refs {
 			fmt.Fprintln(w, v)
 		}
 		return
 	}
 
-	if len(refs) == 0 {
-		fmt.Fprintln(w, "No references found.")
-		return
-	}
+	table := getTable(cmd, refs, h)
 
+	switch strings.ToLower(path.Ext(cmd.OutFile)) {
+	case xlsxExt:
+		writeXlsx(cmd.OutFile, table)
+	case csvExt:
+		writeCSV(cmd.OutFile, table)
+	default:
+		w := os.Stdout
+		if cmd.OutFile != "" {
+			f, err := os.Create(cmd.OutFile)
+			if err != nil {
+				fmt.Println("could not create file")
+				return
+			}
+			defer func() {
+				f.Sync()
+				f.Close()
+			}()
+			w = f
+		}
+		writeFile(w, dmpPath, table)
+	}
+}
+
+func writeFile(w io.Writer, dmpPath string, table [][]string) {
 	fmt.Fprintf(w, "Device external references\n\n  Source device: %s\n\n", dmpPath)
+	printTable(w, table, false)
+}
 
+func getTable(cmd *Command, refs []string, h *refHandler) [][]string {
+	f := withoutSource
 	if cmd.Sources {
+		f = withSource
 		if cmd.ShowType {
-			printWithSourceAndType(w, h)
-			return
+			f = withSourceAndType
 		}
-		printWithSource(w, h)
+	}
+	return f(refs, h)
+}
+
+func withoutSource(refs []string, h *refHandler) [][]string {
+
+	table := make([][]string, 0, len(h.refs))
+	table = append(table, []string{"External Reference", "Count"})
+
+	for _, v := range refs {
+		table = append(table, []string{v, fmt.Sprintf("%5d", len(h.refs[v]))})
+	}
+	return table
+}
+
+func withSourceAndType(refs []string, h *refHandler) [][]string {
+
+	table := make([][]string, 0, len(h.refs))
+	table = append(table, []string{"External Reference", "Source", "Type Name"})
+
+	for _, v := range refs {
+		for _, do := range h.refs[v] {
+			table = append(table, []string{v, do.Path, do.Type})
+		}
+	}
+	return table
+}
+
+func withSource(refs []string, h *refHandler) [][]string {
+
+	table := make([][]string, 0, len(h.refs))
+	table = append(table, []string{"External Reference", "Source"})
+	for _, v := range refs {
+		for _, do := range h.refs[v] {
+			table = append(table, []string{v, do.Path})
+		}
+	}
+	return table
+}
+
+func printTable(w io.Writer, table [][]string, elide bool) {
+
+	col_sep := " "
+	col_pad := " "
+
+	widths := lens(table)
+
+	{
+		// print header
+		pad := ""
+		for i, col := range table[0] {
+			fmt.Fprintf(w, "%s%s", pad, col)
+			pad = strings.Repeat(col_pad, widths[i]-len(col)) + col_sep
+		}
+		fmt.Fprintln(w)
+	}
+
+	{
+		// print header line
+		pad := ""
+		for _, col := range widths {
+			fmt.Fprintf(w, "%s%s", pad, strings.Repeat("-", col))
+			pad = col_sep
+		}
+		fmt.Fprintln(w)
+	}
+
+	ref := ""
+	for _, row := range table[1:] {
+		pad := ""
+		for i, col := range row {
+			if elide && i == 0 && ref == col {
+				col = "-"
+			}
+			fmt.Fprintf(w, "%s%s", pad, col)
+			pad = strings.Repeat(col_pad, widths[i]-len(col)) + col_sep
+		}
+		fmt.Fprintln(w)
+		ref = row[0]
+	}
+}
+
+func lens(table [][]string) []int {
+	widths := make([]int, len(table[1]))
+	for _, row := range table {
+		for i, col := range row {
+			n := len(col)
+			if widths[i] < n {
+				widths[i] = n
+			}
+		}
+	}
+	return widths
+}
+
+func writeCSV(fileName string, table [][]string) {
+
+	w, err := os.Create(fileName)
+	if err != nil {
+		fmt.Println("could not create file")
 		return
 	}
-	printWithoutSource(w, h)
+	defer func() {
+		w.Sync()
+		w.Close()
+	}()
+
+	csvW := csv.NewWriter(w)
+
+	header := table[0]
+	if err := csvW.Write(header); err != nil {
+		log.Fatalln("error writing record to csv:", err)
+	}
+
+	for _, row := range table[1:] {
+		if err := csvW.Write(row); err != nil {
+			log.Fatalln("error writing record to csv:", err)
+		}
+	}
+	csvW.Flush()
 }
 
-func printWithoutSource(w io.Writer, h *refHandler) {
-	refs := maps.Keys(h.refs)
-	slices.Sort(refs)
+func writeXlsx(fName string, table [][]string) {
 
-	wRef := len("External Reference")
-	for _, v := range refs {
-		n := len(v)
-		if wRef < n {
-			wRef = n
+	ws := lens(table)
+
+	f := excelize.NewFile()
+	defer func() {
+		if err := f.Close(); err != nil {
+			fmt.Println(err)
 		}
+	}()
+
+	for i, col := range ws {
+		f.SetColWidth("Sheet1", fmt.Sprintf("%c", 'A'+i), fmt.Sprintf("%c", 'A'+i), float64(col))
 	}
 
-	fmt.Fprintf(w, "%s%s %5s\n", "External Reference", strings.Repeat(" ", wRef-len("External Reference")), "Count")
-	fmt.Fprintf(w, "%s%s %5s\n", strings.Repeat("-", wRef), "", "-----")
-	for _, v := range refs {
-		fmt.Fprintf(w, "%s%s %5d\n", v, strings.Repeat(" ", wRef-len(v)), len(h.refs[v]))
-	}
-}
-
-func printWithSourceAndType(w io.Writer, h *refHandler) {
-
-	refs := maps.Keys(h.refs)
-	slices.Sort(refs)
-
-	wRef := len("External Reference")
-	for _, v := range refs {
-		n := len(v)
-		if wRef < n {
-			wRef = n
-		}
+	for i, row := range table {
+		f.SetSheetRow("Sheet1", fmt.Sprintf("%c%d", 'A', i+1), &row)
 	}
 
-	wSrc := 0
-	for _, dos := range h.refs {
-		for _, do := range dos {
-			n := len(do.Path)
-			if wSrc < n {
-				wSrc = n
-			}
-		}
-	}
-
-	fmt.Fprintf(w, "%s%s %s%s %s\n", "External Reference", strings.Repeat(" ", wRef-len("External Reference")), "Source", strings.Repeat(" ", wSrc-len("Source")), "Type Name")
-	fmt.Fprintf(w, "%s %s %s\n", strings.Repeat("-", wRef), strings.Repeat("-", wSrc), "----------------")
-	for _, v := range refs {
-		dos := h.refs[v]
-		fmt.Fprintf(w, "%s%s %s%s %s\n", v, strings.Repeat(" ", wRef-len(v)), dos[0].Path, strings.Repeat(" ", wSrc-len(dos[0].Path)), dos[0].Type)
-		for _, do := range dos[1:] {
-			fmt.Fprintf(w, "-%s%s %s%s %s\n", strings.Repeat(" ", len(v)-1), strings.Repeat(" ", wRef-len(v)), do.Path, strings.Repeat(" ", wSrc-len(do.Path)), do.Type)
-		}
-	}
-
-}
-
-func printWithSource(w io.Writer, h *refHandler) {
-
-	refs := maps.Keys(h.refs)
-	slices.Sort(refs)
-
-	wRef := len("External Reference")
-	for _, v := range refs {
-		n := len(v)
-		if wRef < n {
-			wRef = n
-		}
-	}
-
-	wSrc := 0
-	for _, dos := range h.refs {
-		for _, do := range dos {
-			n := len(do.Path)
-			if wSrc < n {
-				wSrc = n
-			}
-		}
-	}
-
-	fmt.Fprintf(w, "%s%s %s\n", "External Reference", strings.Repeat(" ", wRef-len("External Reference")), "Source")
-	fmt.Fprintf(w, "%s %s\n", strings.Repeat("-", wRef), strings.Repeat("-", wSrc))
-	for _, v := range refs {
-		dos := h.refs[v]
-		fmt.Fprintf(w, "%s%s %s\n", v, strings.Repeat(" ", wRef-len(v)), dos[0].Path)
-		for _, do := range dos[1:] {
-			fmt.Fprintf(w, "-%s%s %s\n", strings.Repeat(" ", len(v)-1), strings.Repeat(" ", wRef-len(v)), do.Path)
-		}
+	if err := f.SaveAs(fName); err != nil {
+		fmt.Println(err)
 	}
 }
